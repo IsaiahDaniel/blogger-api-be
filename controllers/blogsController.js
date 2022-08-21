869e9bc21b20f72477b000e95903e3decc3992fb
Isaiah Daniel <isaiahdaniel203@gmail.com>
2022-08-21 12:52:34 +0100

    Added getBlog route

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -7,27 +7,23 @@ import Blog from "../models/blogModel.js";
 // @desc        private
 
 const getBlogs = asyncHandler(async (req, res) => {
-  const blogs = [
-    {
-      id: 3,
-      title: "An Introduction to angular",
-      content: "Lorem ipsum dolor sit amet",
-    },
-    {
-      id: 2,
-      title: "An Introduction to React",
-      content: "Lorem ipsum dolor sit amet",
-    },
-    {
-      id: 3,
-      title: "An Introduction to Vue",
-      content: "Lorem ipsum dolor sit amet",
-    },
-  ];
-
-  // const blogs = await Blog.find();
-
+  const blogs = await Blog.find();
   res.status(200).json({ success: true, data: blogs, count: blogs.length });
 });
 
-export { getBlogs };
+// @route       /api/v1/blogs/:id
+// @desc        Get single Blog
+// @desc        private
+
+const getBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findOne({ id: req.params.id });
+
+  if(!blog){
+    return next(new ErrorResponse(`No blog with the id ${req.params.id}`));
+  }
+
+  res.status(200).json({ success: true, data: blog });
+});
+
+
+export { getBlogs, getBlog };
